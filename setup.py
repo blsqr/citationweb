1aedfbfcacde0d4fcef2f97001c723717e385045
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-05-19 11:27:52 +0200

    Update setup.py to include the correct dependencies

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python3
+"""Set up the citationweb"""
+
 from setuptools import setup
 
-setup(name='citationweb',
-      version='0.1',
-      description='A little tool to go through a BibDesk library file, extract citations from the attached PDF files and create a network of citations.',
-      url='',
-      author='Yunus Sevinchan',
-      author_email='yunussevinchan@gmail.com',
-      license='',
-      packages=['citationweb'],
-      install_requires=['pypdf2', 'pybtex'],
-      dependency_links=[
-        'https://github.com/CrossRef/pdfextract', # is a ruby gem
-        'https://git.skewed.de/count0/graph-tool', # has to be built
-      ],
-      zip_safe=False)
+INSTALL_DEPS = ["PyYAML>=3.12", "pypdf2>=1.26", "pybtex>=0.21"]
+TEST_DEPS    = ["pytest>=3.4.0", "pytest-cov>=2.5.1"]
+
+setup(name="citationweb",
+      version="1.0-alpha",
+      description="Processes BibTex files and creates a network of citations",
+      url="https://github.com/blusquare/citationweb",
+      author="Yunus Sevinchan",
+      author_email="blsqr0@gmail.com",
+      license="", # TODO
+      packages=["citationweb"],
+      install_requires=INSTALL_DEPS,
+      tests_require=TEST_DEPS,
+      test_suite="pytest",
+      extras_require=dict(test_deps=TEST_DEPS),
+      scripts=["cli/cweb"]
+      )
