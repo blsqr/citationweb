735e5419d1d19ea7638371f3c7c071d555054c0c
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-05-19 12:53:40 +0200

    Add test *.bib files and do a BibDesk-file test

--- tests/test_bibliography.py
+++ tests/test_bibliography.py
@@ -20,18 +20,34 @@ def bib_minimal(tmpdir) -> str:
     copyfile(src, dst)
     return str(dst)
 
+@pytest.fixture
+def bib_bibdesk(tmpdir) -> str:
+    """Returns the path to a BibDesk bibliography file that is copied to a
+    temporary directory."""
+    src = resource_filename("tests", "libs/bibdesk.bib")
+    dst = tmpdir.join("tmp.bib")
+
+    # Copy the file to the temporary directory and return the path
+    copyfile(src, dst)
+    return str(dst)
+
 # Tests -----------------------------------------------------------------------
 
-def test_init(bib_minimal):
+def test_init(bib_minimal, bib_bibdesk):
     """Test the Bibliograpy class initialisation"""
     bib = Bibliography(bib_minimal)
 
     # Assert the file and the data is loaded
     assert bib.file
     assert bib.data
+    assert bib.appdx is None
 
     # Test different creators
-    Bibliography(bib_minimal, creator='BibDesk')
+    bd_bib = Bibliography(bib_bibdesk, creator='BibDesk')
+
+    assert bd_bib.file
+    assert bd_bib.data
+    assert bd_bib.appdx
 
     # Assert failure for unsupported creators
     with pytest.raises(ValueError, match="Unsupported creator 'invalid'"):
