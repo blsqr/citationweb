deaa69e76c501673f86063970cee3080e197063c
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-05-19 11:32:43 +0200

    Remove imports from __init__

--- citationweb/__init__.py
+++ citationweb/__init__.py
@@ -1,6 +1,3 @@
-"""This little package provides functions to read and parse a bibtex library, and work on the cited-by and cites keys to create a web of citations"""
-
-# import all public (!) functions
-from .functions import *
-from .crosslink import *
-from .network import *
+"""citationweb is a collection of tools to parse a bibtex file and generate
+a network of citations from it
+"""
