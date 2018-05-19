3bbd024d4989daa8572741b12ff026de11d9256d
Yunus Sevinchan <YunusSevinchan@gmail.com>
2018-05-19 11:33:12 +0200

    Add pytest suite

---
+++ tests/test_bibdata.py
@@ -0,0 +1,14 @@
+"""Test the BibData class"""
+
+import pytest
+
+import citationweb as cweb
+
+# Fixtures --------------------------------------------------------------------
+
+
+# Tests -----------------------------------------------------------------------
+
+@pytest.mark.skip("Nothing to test yet.")
+def test_init():
+    pass
