d376acadd53347858bfca2757a27eda3945ec166
Yunus Sevinchan <YunusSevinchan@gmail.com>
2019-07-10 19:45:18 +0200

    Configure travis to test for python 3.7 and add networkx dependency

--- setup.py
+++ setup.py
@@ -4,29 +4,34 @@
 from setuptools import setup
 
 INSTALL_DEPS = [
-                "PyYAML>=3.12",
-                "pypdf2>=1.26",
-                "pybtex>=0.21",
-                "requests>=2.18"
-                ]
-TEST_DEPS = ["pytest>=3.4.0", "pytest-cov>=2.5.1"]
+    "PyYAML>=3.12",
+    "pypdf2>=1.26",
+    "pybtex>=0.21",
+    "requests>=2.18",
+    "networkx>=2.3"
+]
+TEST_DEPS = [
+    "pytest>=3.4.0",
+    "pytest-cov>=2.5.1"
+]
 
-setup(name="citationweb",
-      version="1.0-alpha",
-      description="Processes BibTex files and creates a network of citations",
-      url="https://github.com/blusquare/citationweb",
-      author="Yunus Sevinchan",
-      author_email="blsqr0@gmail.com",
-      license="", # TODO
-      packages=["citationweb"],
-      package_data=dict(citationweb=["*.yml"]),
-      install_requires=INSTALL_DEPS,
-      tests_require=TEST_DEPS,
-      test_suite="pytest",
-      extras_require=dict(test_deps=TEST_DEPS),
-      scripts=["cli/cweb"],
-      dependency_links=[
-          # pdf-extract ruby gem
-          "https://github.com/CrossRef/pdfextract"
-      ]
-      )
+setup(
+    name="citationweb",
+    version="1.0-alpha",
+    description="Processes BibTex files and creates a network of citations",
+    url="https://github.com/blusquare/citationweb",
+    author="Yunus Sevinchan",
+    author_email="blsqr0@gmail.com",
+    license="", # TODO
+    packages=["citationweb"],
+    package_data=dict(citationweb=["*.yml"]),
+    install_requires=INSTALL_DEPS,
+    tests_require=TEST_DEPS,
+    test_suite="pytest",
+    extras_require=dict(test_deps=TEST_DEPS),
+    scripts=["cli/cweb"],
+    dependency_links=[
+        # pdf-extract ruby gem
+        "https://github.com/CrossRef/pdfextract"
+  ]
+)
